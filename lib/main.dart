7e1fc79aed04d7dfc30e2cc8e6ce88b903b59349
AtrVorobev <artvorobev96@gmail.com>
2025-02-26 18:16:40 +0400

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:openvpn_flutter/openvpn_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (_) => VpnState(),
+        child: MaterialApp(home: HomeScreen()),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _HomeScreenState extends State<HomeScreen> {
+  final OpenVPN _openVpn = OpenVPN();
+  bool _isInitialized = false;
+  VpnStatus _vpnStatus = VpnStatus.empty();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    _initialize();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+   Future<void> _initialize() async {
+    await _openVpn.initialize(
+      lastStatus: (status) => setState(() => _vpnStatus = status),
+    );
+    setState(() => _isInitialized = true);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text('VPN App')),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: !_isInitialized
+            ? CircularProgressIndicator()
+            : Consumer<VpnState>(
+                builder: (context, state, _) => Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildStatusText(state.status),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => _toggleVpn(state),
+                      child: Text(state.isConnected ? 'Disconnect' : 'Connect'),
+                    ),
+                  ],
+                ),
+              ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildStatusText(VpnStatus status) {
+    return Text(
+      status.toString().split('.').last,
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Future<void> _toggleVpn(VpnState state) async {
+    print('_toggleVpn');
+
+    if (state.isConnected) {
+      _openVpn.disconnect();
+    } else {
+      _startVpn();
+    }
+  }
+
+  Future<void> _startVpn() async {
+    bool isConnected = await _checkPermissions();
+
+    if (!await _checkPermissions()) return;
+
+    String config = await rootBundle.loadString('assets/config.ovpn');
+
+    await _openVpn.connect(
+      config,
+      'open_vpn',
+      username: 'vpn',
+      password: 'vpn',
+      // onConnectionStatusChanged: (status) {
+      //   print('Connection Status: $status');
+      // },
+    );
+  }
+
+  Future<bool> _checkPermissions() async {
+    bool location = await Permission.location.request().isGranted;
+
+    print('location $location');
+
+    if (location) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  void dispose() {
+    _openVpn.disconnect();
+    super.dispose();
+  }
+}
+
+
+class VpnState with ChangeNotifier {
+  VpnStatus _status = VpnStatus.empty();
+  bool _isConnected = false;
+
+  VpnStatus get status => _status;
+  bool get isConnected => _isConnected;
+
+  // Метод для обновления статуса
+  void updateStatus(VpnStatus status) {
+    _status = status;
+    _isConnected = status.connectedOn != null; // Проверка на подключение
+    notifyListeners();
   }
 }
